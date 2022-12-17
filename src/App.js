5b24e4b9fe48e0a3c0b7c8d1e383500b65251a01
kursatoguz <kursatoguz07@hotmail.com>
2022-12-17 05:36:48 +0300

    header completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+const apiKey = "at_cTk1HcsArO2k4YxTohlSdHfi7tVDV";
 
 function App() {
+  const [ip, setIp] = useState("");
+
+  useEffect(() => {
+    async function getIp() {
+      const response = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`
+      );
+      const data = await response.json();
+      setIp(data.ip);
+    }
+    getIp();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>IP Adress Tracker</h1>
+      <div className="search-bar">
+        <input placeholder="Search for any IP adress or domain" />
+        <span className="icon"> {">"} </span>
+      </div>
+    </header>
   );
 }
 
